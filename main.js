a53b0602f473424e9993ccd7d6d18629d62d0223
badwulfy <3044771+badwulfy@users.noreply.github.com>
2023-11-22 19:30:47 +0000

    Codesandbox config

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ import "./style.css";
 // Récupérez l'élément d'entrée de rayon et l'élément du cercle
 const radiusInput = document.getElementById("radius");
 const circle = document.getElementById("circle");
-const radius = document.getElementById("radius");
 
 const updateCircle = (event) => {
-  const radiusValue = Math.max(100,radiusInput.value);
-  circle.style.left = (event.clientX - radiusValue)  + "px";
-  circle.style.top = (event.clientY - radiusValue ) + "px";
+  const radiusValue = Math.max(100, radiusInput.value);
+  circle.style.left = event.clientX - radiusValue + "px";
+  circle.style.top = event.clientY - radiusValue + "px";
   circle.style.width = radiusValue * 2 + "px";
   circle.style.height = radiusValue * 2 + "px";
 };
